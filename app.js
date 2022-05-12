3da990b3c072688e34b053b4cfa5b781389be40e
Cooper Reid <creid@mparticle.com>
2022-05-12 17:54:24 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+const {validate} = require('./lib/mparticle-dataplan-client');
+
+// Declare a route
+fastify.post('/', async (request, reply) => {
+  // track event to mParticle
+  const check = await validate(
+    request.body, 
+    dataPlanId=request.headers['mp-dataplanid'], 
+    dataPlanVersion=request.headers['mp-dataplanversion'], 
+    dataPlanTTL=request.headers['mp-dataplanttl']);
+        
+  return check;
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start();
